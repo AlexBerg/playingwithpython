d06ef916a3eb89dd5044c3683fc8c6d9abf7e3bf
Alexander Bergersen <alex@Alexanders-MacBook-Pro.local>
2018-10-02 21:29:10 +0200

    -this is starting to go of the deep end

--- ecc/utilities.py
+++ ecc/utilities.py
@@ -1,6 +1,24 @@
+def memoize(f):
+    cache = {}
+
+    def memo(*args, **kwargs):
+        argTuple = args + tuple(kwargs)
+        if argTuple not in cache:
+            cache[argTuple] = f(*args, **kwargs)
+        return cache[argTuple]
+
+    memo.cache = cache
+    return memo
+
+
+
 # A typechecker for trying to cast arguments to same class
 def typecheck(f):
     def newF(self, other):
+        if (hasattr(other.__class__, 'operatorPrecedence') and
+                other.__class__.operatorPrecedence > self.__class__.operatorPrecedence):
+                return NotImplemented
+
         if type(self) is not type(other):
             try:
                 other = self.__class__(other)
@@ -17,6 +35,7 @@ def typecheck(f):
 
 # So we dont need to overload for all numbertypes
 class DomainElement(object):
+    operatorPrecedence = 1
     def __radd__(self, other):
         return self + other
     def __rsub__(self, other):

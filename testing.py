52ffc3092b24cb6f8f57095452b3fd7b8a761062
Alexander Bergersen <alex@Alexanders-MacBook-Pro.local>
2019-01-04 00:12:33 +0100

    At least it gets futher now in the test

--- testing.py
+++ testing.py
@@ -1,12 +1,14 @@
 from ecc.elgamal import Elgamal
 from ecc.curves import P192
-from ecc.fieldInts import FieldInts
+from ecc.mods import Mod
 
 
 def testElgamal():
     el = Elgamal(P192)
+    print("Have curve!")
     clr = 7
     pub = el.generateKey()
+    print("Generated key!")
     cipher = el.encrypt(clr, pub[0], pub[1], pub[2])
     decrypted = el.decrypt(cipher)
     if decrypted == clr:
@@ -16,7 +18,7 @@ def testElgamal():
 
 
 def testField():
-    mod7 = FieldInts(7)
+    mod7 = Mod(7)
     result = mod7(3) + mod7(6)
     if result == mod7(2):
         return True

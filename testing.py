a35050f8f17f67cf9bd033916935e4335af83a3b
Alexander Bergersen <bergersen.alexander@gmail.com>
2019-04-28 18:10:59 +0200

    switched testing to unittest

--- testing.py
+++ testing.py
@@ -2,6 +2,26 @@ from ecc.elgamal import Elgamal
 from ecc.curves import P192
 from ecc.mods import Mod
 from ecc.innercircle import InnerCircle
+import unittest
+
+class Test(unittest.TestCase):    
+    def test_elgamal(self):
+        el = Elgamal(P192)
+        clr = 7
+        pub = el.generateKey()
+        cipher = el.encrypt(clr, pub, P192.G, P192.P)
+        decrypted = el.decrypt(cipher)
+        clrpoint = P192.getNumberAsCurvePoint(clr)
+
+        self.assertEqual(decrypted, clrpoint, "Should be point for 7")
+
+    def test_mod(self):
+        mod7 = Mod(7)
+        result = mod7(3) + mod7(6)
+        self.assertEqual(result, mod7(2), "Should be 2 mod 7")
+
+    def test_inner(self):
+        self.assertTrue(testInnerCircle(), "Should be true")
 
 
 def testInnerCircle():
@@ -19,43 +39,5 @@ def testInnerCircle():
 
 
 
-def testElgamal():
-    el = Elgamal(P192)
-    clr = 7
-    pub = el.generateKey()
-    cipher = el.encrypt(clr, pub, P192.G, P192.P)
-    decrypted = el.decrypt(cipher)
-    clrpoint = P192.getNumberAsCurvePoint(clr)
-    if decrypted == clrpoint:
-        return True
-    else:
-        return False
-
-
-def testMod():
-    mod7 = Mod(7)
-    result = mod7(3) + mod7(6)
-    if result == mod7(2):
-        return True
-    else:
-        return False
-
-
 if __name__ == "__main__":
-    result = testMod()
-    if result:
-        print("Mod Success")
-    else:
-        print("Mod Failed")
-
-    result = testElgamal()
-    if result:
-        print("Elgamal Success")
-    else:
-        print("Elgamal Failed")
-
-    result = testInnerCircle()
-    if result:
-        print("InnerCircle Success")
-    else:
-        print("InnerCircle Failed")
+    unittest.main()
